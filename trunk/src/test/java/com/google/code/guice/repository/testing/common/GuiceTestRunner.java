4dad8743bffa448c3d844eaa3e987b3b27d41bbd
Aleksey.Krylov@gmail.com <Aleksey.Krylov@gmail.com@2f7b4ad1-e6e1-6709-36a2-3d7a233e2e58>
2012-11-05 15:47:07 +0000

    [+] milestone commit

--- trunk/src/test/java/com/google/code/guice/repository/testing/common/GuiceTestRunner.java
+++ trunk/src/test/java/com/google/code/guice/repository/testing/common/GuiceTestRunner.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package com.google.code.guice.repository.common;
+package com.google.code.guice.repository.testing.common;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;

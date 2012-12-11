0f3ebe0912d5985b402579b1c497f1bcc7b632fa
aleksey.krylov@gmail.com <aleksey.krylov@gmail.com@2f7b4ad1-e6e1-6709-36a2-3d7a233e2e58>
2012-12-11 09:51:58 +0000

    [+] pom cleanup

--- src/main/java/com/google/code/guice/repository/filter/PersistFilter.java
+++ src/main/java/com/google/code/guice/repository/filter/PersistFilter.java
@@ -1,6 +1,19 @@
 /*
- * Copyright (c) 2012, i-Free. All Rights Reserved.
- * Use is subject to license terms.
+ * Copyright (C) 2012 the original author or authors.
+ * See the notice.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.google.code.guice.repository.filter;
@@ -34,7 +47,11 @@ public class PersistFilter extends OpenEntityManagerInViewFilter {
 
     @Override
     protected EntityManagerFactory lookupEntityManagerFactory(HttpServletRequest request) {
-        String persistenceUnitName = request.getParameter(P_PERSISTENCE_UNIT_NAME);
+        String persistenceUnitName = getPersistenceUnitName();
+        if(persistenceUnitName == null) {
+            persistenceUnitName = request.getParameter(P_PERSISTENCE_UNIT_NAME);
+        }
+        // persistenceUnitName can still be null - in this case default configuration will be used
         return configurationManager.getConfiguration(persistenceUnitName).getEntityManagerFactory();
     }
 
